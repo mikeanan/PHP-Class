7bb3ad88129969a493a46e832e090b6319e51e6e
mike <chennanbang@gmail.com>
2017-11-02 10:59:48 +0800

    靜態屬性, 方法 的宣告及使用

--- AllClass/Cat.php
+++ AllClass/Cat.php
@@ -5,16 +5,23 @@ include_once "Animal.php";
 class Cat extends Animal
 {
     private $petName;
+    public static $staticProperty = "靜態屬性";//宣告同時給一個值
 
     public function __construct($petNameFromOut) {
         parent::__construct("貓");//需要呼叫父類別初始化的方法, 否則父類別不會做他的建構子的初始動作
         $this->petName = $petNameFromOut;
 //        var_dump($this->position);
+        self::$staticProperty = "在建構子中設定(".$this->petName.")";//要用 self:: 來存取靜態屬性
     }
 
     public function getPetName() {
 //        $this->getAnimalType();//測試 protected 方法
         return $this->petName;
+    }
+
+    public static function staticFunction($test) {
+        echo "this is static function";
+        self::$staticProperty = "從靜態方法改變靜態屬性(".$test.")";
     }
 }
 
