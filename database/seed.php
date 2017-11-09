56fac6ae19be80dd0fe96c442cc7100f48b5fe1d
mike <chennanbang@gmail.com>
2017-11-09 16:37:52 +0800

    將假資料寫入資料庫

--- database/seed.php
+++ database/seed.php
@@ -6,11 +6,24 @@
 include_once __DIR__."/../vendor/autoload.php";
 include_once "read.php";
 
+use Illuminate\Database\Capsule\Manager as Capsule;
+
 $faker = Faker\Factory::create();
 
-echo "<h3>$faker->name</h3>";
-echo "<h4>$faker->address</h4>";
-echo $faker->text."<br>";
+//echo "<h3>$faker->name</h3>";
+//echo "<h4>$faker->address</h4>";
+//echo $faker->text."<br>";
+
+for($i=0; $i<5; $i++){
+    Capsule::table('users')->insert(
+        [
+            'name' => $faker->name,
+            'phone' => $faker->phoneNumber,
+            "address" => $faker->address,
+            "email" => $faker->email
+        ]
+    );
+}
 
 ?>
 
